3564d40e8257483d8645ef96757f5522eab79709
theisaachome <isaachome.burma@gmail.com>
2021-03-29 15:35:10 +0630

    converting error to response

---
+++ auth/src/index.ts
@@ -0,0 +1,21 @@
+import express, { json } from 'express';
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+import {signupRouter} from './routes/signup';
+import {errorHandler}from './middlewares/error-handler';
+
+const app = express();
+app.use(express.json());
+
+app.use(currentUserRouter);
+
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!!!!!!');
+});
---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.post("/api/users/signin",(req,res)=>{
+    res.send("Hi there from signin");
+});
+
+export { router as signinRouter};
