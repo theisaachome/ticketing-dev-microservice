b4aebb2f16fc5b7d55a74b16a4c8534812831f25
theisaachome <isaachome.burma@gmail.com>
2021-03-30 22:11:28 +0630

    section:7 finish

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express, { json } from 'express';
+import 'express-async-errors';
 import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
 import {signupRouter} from './routes/signup';
 import {errorHandler}from './middlewares/error-handler';
+
+import {NotFoundError}from './errors/not-found-error';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +17,9 @@ app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 
+app.all("*",async(req,res,next)=>{
+  throw new NotFoundError();
+})
 app.use(errorHandler);
 
 app.listen(3000, () => {
