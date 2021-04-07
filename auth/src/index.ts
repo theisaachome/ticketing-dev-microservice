db524920bd308dbb9099b3f792354f5c09014cc0
theisaachome <isaachome.burma@gmail.com>
2021-04-07 08:37:19 +0630

    authentication done section 10

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,27 +1,21 @@
-import express, { json } from 'express';
-import 'express-async-errors';
-import {currentUserRouter} from './routes/current-user';
-import {signinRouter} from './routes/signin';
-import {signoutRouter} from './routes/signout';
-import {signupRouter} from './routes/signup';
-import {errorHandler}from './middlewares/error-handler';
-
-import {NotFoundError}from './errors/not-found-error';
-
-const app = express();
-app.use(express.json());
-
-app.use(currentUserRouter);
-
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-
-app.all("*",async(req,res,next)=>{
-  throw new NotFoundError();
-})
-app.use(errorHandler);
-
+import  mongoose from 'mongoose';
+import {app} from './app';
+const start= async()=>{
+  if(!process.env.JWT_KEY){
+    throw new Error("JWT_KEY must be defined");
+  }
+    try{ 
+      await mongoose.connect("mongodb://auth-mongo-srv:27017/auth",{
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+      useCreateIndex:true,
+    });
+    console.log("conntece to MongoDB");
+  }catch(err){
+  console.log(err);
+  }
+};
+start();
 app.listen(3000, () => {
   console.log('Listening on port 3000!!!!!!!!');
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express,{Request,Response} from "express";
-import {body,validationResult} from "express-validator";
-import {DatabaseConnectionError} from '../errors/database-connnection-error';
-import {RequestValidationError} from "../errors/request-validation-error";
+import jwt from 'jsonwebtoken';
+import {body} from "express-validator";
+import {BadRequestError} from "../errors/bad-request-error";
+import {validateRequest} from '../middlewares/validate-request';
+import {User} from '../models/user';
 const router = express.Router();
 
 router.post("/api/users/signup",
@@ -13,13 +15,28 @@ router.post("/api/users/signup",
     .trim()
     .isLength({min:4,max:20})
     .withMessage('Password must be between 4 and 20 characters')
-],(req:Request,res:Response)=>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-       throw new RequestValidationError(errors.array());
+],
+validateRequest,
+async(req:Request,res:Response)=>{
+   
+    const {email,password} = req.body;
+
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+      throw new BadRequestError('Email in Used');
     }
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const user =  User.build({email,password});
+    await user.save();
+    // generate jsonwebtoken 
+      const userJwt = jwt.sign({
+        id:user.id,
+        email:user.email,
+      },process.env.JWT_KEY!);
+    //store in on session object
+    req.session= {
+      jwt:userJwt
+    };
+    res.status(201).send(user);
 
 });
 
